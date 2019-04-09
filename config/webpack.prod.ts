f19685244a8577feb477439d78851778ed9214de
zeronight <zeroxiaobai@gmail.com>
2019-04-09 20:33:03 +0800

    build: use terser instead of uglifyjs

--- config/webpack.prod.ts
+++ config/webpack.prod.ts
@@ -3,7 +3,7 @@ import path from 'path';
 import webpack from 'webpack';
 import merge from 'webpack-merge';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import UglifyJsWebpackPlugin from 'uglifyjs-webpack-plugin';
+import TerserWebpackPlugin from 'terser-webpack-plugin';
 import OptimizeCSSAssetsPlugin from 'optimize-css-assets-webpack-plugin';
 import PrerenderSPAPlugin from 'prerender-spa-plugin';
 import { ArticleBase } from 'types/database';
@@ -60,7 +60,7 @@ const config: webpack.Configuration = merge(baseConfig, {
   optimization: {
     minimizer: [
       new OptimizeCSSAssetsPlugin(),
-      new UglifyJsWebpackPlugin({
+      new TerserWebpackPlugin({
         cache: true,
         parallel: true,
         sourceMap: false,
