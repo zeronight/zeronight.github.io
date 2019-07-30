34d490eb0085a39a1db3613c0f39625417c84340
zeronight <zeroxiaobai@gmail.com>
2019-07-30 19:15:24 +0800

    chore: update react-syntax-highlighter to 11.0.2

--- src/components/code-block/index.tsx
+++ src/components/code-block/index.tsx
@@ -1,6 +1,6 @@
 import React, { StatelessComponent } from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
-import github from 'react-syntax-highlighter/dist/styles/hljs/github';
+import github from 'react-syntax-highlighter/dist/esm/styles/hljs/github';
 
 export interface CodeBlockProps {
   value: string;
