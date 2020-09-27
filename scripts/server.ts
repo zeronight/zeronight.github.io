0845d172f9594e1a5e9b3da528e6c47ada9ab54e
xianjiantao <xianjiantao@bytedance.com>
2020-09-27 11:06:19 +0800

    chore(scripts): fix lint

--- scripts/server.ts
+++ scripts/server.ts
@@ -3,7 +3,7 @@ import express from 'express';
 
 const app = express();
 const port = process.env.PORT ? Number(process.env.PORT) : 8080;
-const host = process.env.HOST ? process.env.HOST: '127.0.0.1';
+const host = process.env.HOST ? process.env.HOST : '127.0.0.1';
 
 app.use(express.static(path.resolve(__dirname, '..')));
 
